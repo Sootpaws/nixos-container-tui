2e18cd27093cc0f438a726319cd8cd7c78cf18ac
Sootpaws <sootpaws@proton.me>
2026-02-12 16:24:16 -0500

    Start building TUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use cursive::views::{ListView, ScrollView, Button};
+
 fn main() {
-    println!("Hello, world!");
+    let mut r = cursive::default();
+    r.add_global_callback('q', |s| s.quit());
+    r.add_layer(
+        ScrollView::new(ListView::new()
+            .child("a", Button::new("meow", |_|{}))
+            .child("b", Button::new("mrrp", |_|{}))
+            .child("c", Button::new("nyaa", |_|{}))
+            .child("d", Button::new("meow", |_|{}))
+    ));
+    r.run()
 }
