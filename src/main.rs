de6a70bebb44ed1a88b1e6842a14adc7c0c3f051
Sootpaws <sootpaws@proton.me>
2026-02-14 12:23:21 -0500

    Move TUI construction into a function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use backend::messages::{ContainerState, NamedUpdate, Update};
 use cursive::Cursive;
-use cursive::view::{Nameable, View};
-use cursive::views::{Button, LinearLayout, ListChild, ListView, ScrollView};
+use cursive::view::View;
+use cursive::views::{Button, LayerPosition, LinearLayout, ListChild, ListView, ScrollView};
 use tokio::task;
 
 /// Backend for communicating with systemd over dbus
@@ -12,19 +12,9 @@ async fn main() {
     // Start the backend
     let (mut recv, containers) = backend::start_backend().await.unwrap();
 
-    // Create the list of containers and their controls/info
-    let container_list = {
-        let mut list = ListView::new();
-        for container in &containers {
-            let controls = LinearLayout::horizontal().child(Button::new("[Unknown]", |_| {}));
-            list.add_child(container, controls);
-        }
-        list.with_name(CONTAINER_LIST_NAME)
-    };
-
+    // Create the TUI
     let mut root = cursive::default();
-    root.add_global_callback('q', |s| s.quit());
-    root.add_layer(ScrollView::new(container_list));
+    create_tui(&mut root, &containers);
 
     // Run the Cursive event loop, which checking for and
     // handling backend messages
@@ -54,9 +44,31 @@ async fn main() {
     }
 }
 
+/// Create the TUI
+fn create_tui(root: &mut Cursive, containers: &Vec<String>) {
+    // Create the list of containers and their controls/info
+    let container_list = {
+        let mut list = ListView::new();
+        for container in containers {
+            let controls = LinearLayout::horizontal().child(Button::new("[Unknown]", |_| {}));
+            list.add_child(container, controls);
+        }
+        list
+    };
+
+    // Set up the main TUI
+    root.add_global_callback('q', |s| s.quit());
+    root.add_layer(ScrollView::new(container_list));
+}
+
 /// Update the TUI given a backend message
 fn handle_message(root: &mut Cursive, message: NamedUpdate) {
-    let mut container_list = root.find_name::<ListView>(CONTAINER_LIST_NAME).unwrap();
+    let mut container_list = root
+        .screen_mut()
+        .get_mut(LayerPosition::FromFront(0))
+        .and_then(|v| v.downcast_mut::<ScrollView<ListView>>())
+        .unwrap()
+        .get_inner_mut();
     let controls = get_list_child(&mut container_list, &message.container_name)
         .and_then(|v| v.downcast_mut::<LinearLayout>())
         .unwrap();
@@ -86,8 +98,6 @@ fn handle_message(root: &mut Cursive, message: NamedUpdate) {
         Update::Error(_) => (),
     }
 }
-
-const CONTAINER_LIST_NAME: &str = "container_list";
 
 /// Get mutable access to a ListView item by label
 fn get_list_child<'a>(view: &'a mut ListView, label: &str) -> Option<&'a mut Box<dyn View>> {
