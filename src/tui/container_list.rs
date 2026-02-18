f775e6fd8d489263492e5ba98514dd105e68d1a4
Sootpaws <sootpaws@proton.me>
2026-02-17 19:41:55 -0500

    Wrap container list in a panel

--- src/tui/container_list.rs
+++ src/tui/container_list.rs
@@ -1,11 +1,11 @@
 use super::ContainerControls;
 use super::utils;
 use cursive::view::ViewWrapper;
-use cursive::views::{ListView, ScrollView};
+use cursive::views::{ListView, Panel, ScrollView};
 
 /// Wrapper for the main container list
 pub struct ContainerList {
-    inner: ScrollView<ListView>,
+    inner: Panel<ScrollView<ListView>>,
 }
 
 impl ContainerList {
@@ -16,17 +16,17 @@ impl ContainerList {
             list.add_child(container, ContainerControls::new());
         }
         Self {
-            inner: ScrollView::new(list),
+            inner: Panel::new(ScrollView::new(list)).title("Containers"),
         }
     }
 
     /// Get the container view for a given name
     pub fn get_container(&mut self, name: &str) -> Option<&mut ContainerControls> {
-        utils::get_list_child(self.inner.get_inner_mut(), name)
+        utils::get_list_child(self.inner.get_inner_mut().get_inner_mut(), name)
             .and_then(|v| v.downcast_mut::<ContainerControls>())
     }
 }
 
 impl ViewWrapper for ContainerList {
-    cursive::wrap_impl!(self.inner: ScrollView<ListView>);
+    cursive::wrap_impl!(self.inner: Panel<ScrollView<ListView>>);
 }
